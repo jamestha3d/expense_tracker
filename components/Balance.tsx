297554c250707158210bca45f35feb1a853c1651
jamestha3d <jamesikediashi@gmail.com>
2024-07-14 02:37:39 -0400

    deployed

---
+++ components/Balance.tsx
@@ -0,0 +1,11 @@
+import getuserBalance from "@/app/actions/getUserBalance";
+import { addCommas } from "@/lib/utils";
+const Balance = async () => {
+    const {balance} = await getuserBalance();
+    return ( <>
+        <h4> Your Balance</h4>
+        <h1> ${ addCommas(Number(balance?.toFixed(2) ?? 0))} </h1>
+    </>);
+}
+ 
+export default Balance;
---
+++ lib/checkUser.ts
@@ -0,0 +1,41 @@
+import { currentUser } from "@clerk/nextjs/server";
+
+import { db } from "./db";
+
+export const checkUser = async () => {
+    const user = await currentUser();
+    console.log(user)
+
+    //  Check for current logged in clerk user
+
+    if (!user) {
+        return null
+    }
+
+    // Check if the user is already in the db
+    const loggedInUser = await db.user.findUnique({
+        where: {
+            clerkUserId: user.id
+        }
+    });
+
+    // If user is in db, return user
+    if(loggedInUser) {
+        return loggedInUser
+    }
+
+    //If not in db, create new user
+
+    const newUser = await db.user.create(
+        {
+            data: {
+                clerkUserId: user.id,
+                name: `${user.firstName} ${user.lastName}`,
+                imageUrl: user.imageUrl,
+                email: user.emailAddresses[0].emailAddress
+            }
+        }
+    );
+
+    return newUser;
+}
